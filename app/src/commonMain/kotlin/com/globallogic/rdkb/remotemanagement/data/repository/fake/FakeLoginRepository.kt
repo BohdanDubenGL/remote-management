663e88de72a041d8c63d287689cb89837865d555
bohdan.duben <bohdan.duben@globallogic.com>
2025-01-27 13:11:27 +0200

    [RM-5] added fake data

--- app/src/commonMain/kotlin/com/globallogic/rdkb/remotemanagement/data/repository/fake/FakeLoginRepository.kt
+++ app/src/commonMain/kotlin/com/globallogic/rdkb/remotemanagement/data/repository/fake/FakeLoginRepository.kt
@@ -4,9 +4,29 @@ import com.globallogic.rdkb.remotemanagement.domain.entity.LoginData
 import com.globallogic.rdkb.remotemanagement.domain.entity.RegistrationData
 import com.globallogic.rdkb.remotemanagement.domain.entity.User
 import com.globallogic.rdkb.remotemanagement.domain.repository.LoginRepository
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
-class FakeLoginRepository : LoginRepository {
-    override suspend fun register(registrationData: RegistrationData): User = User.empty
-    override suspend fun login(loginData: LoginData): User = User.empty
-    override suspend fun logout() = Unit
+class FakeLoginRepository
+@OptIn(ExperimentalUuidApi::class) constructor(
+    private val users: MutableMap<String, User> = mutableMapOf(
+        "user@user.com" to User(Uuid.random(), "user", "user@user.com"),
+    ),
+    private var currentUser: User? = User(Uuid.random(), "user", "user@user.com")
+) : LoginRepository {
+    @OptIn(ExperimentalUuidApi::class)
+    override suspend fun register(registrationData: RegistrationData): User {
+        if (registrationData.email in users) return User.empty
+        val user = User(Uuid.random(), registrationData.username, registrationData.email)
+        users[registrationData.email] = user
+        return user
+    }
+
+    override suspend fun login(loginData: LoginData): User {
+        return users.getOrElse(loginData.email) { User.empty }
+    }
+
+    override suspend fun logout() {
+        currentUser = null
+    }
 }
